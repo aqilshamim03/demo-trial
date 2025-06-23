0850b11a395e3fe725b19eac1823b9b7618db086
aqilshamim03 <aqilshamim5@gmail.com>
2025-06-23 21:52:59 +0530

    username

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
 height_cm = float(input('height coverter:enter your height in cm:'))
 print('your height in feet is:', height_cm / 30.48)
 print('hi, your height is', height_cm, 'and in feet is', height_cm / 30.48)
+username = 'Aqil'

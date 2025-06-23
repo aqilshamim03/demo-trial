6bfca47ec89f4d901cea7fd6392351d779a8b77e
aqilshamim03 <aqilshamim5@gmail.com>
2025-06-23 19:39:23 +0530

    print()

--- demo.py
+++ demo.py
@@ -1,2 +1,3 @@
 height_cm = float(input('height coverter:enter your height in cm:'))
 print('your height in feet is:', height_cm / 30.48)
+print('hi, your height is', height_cm, 'and in feet is', height_cm / 30.48)
